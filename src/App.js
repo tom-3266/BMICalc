267679875346c2d5361189f737bb7609cd937567
tom <tomsibimattathil@gmail.com>
2021-08-28 13:56:10 +0530

    tom/folderStructure/createReactApp

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import CalculateBMI from "./Components/BMICalc";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <CalculateBMI />
+      </header>
+    </div>
+  );
+}
+
+export default App;
